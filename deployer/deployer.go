f7171f9166e9fc8ae6f72d41792cf7bf76d022dd
Simon Walker <swalker@pixsystem.com>
2021-04-09 18:33:06 +0100

    Supports waiting for stack completion

--- deployer/deployer.go
+++ deployer/deployer.go
@@ -5,10 +5,12 @@ import (
 	"errors"
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/cloudformation"
 	"github.com/aws/aws-sdk-go-v2/service/cloudformation/types"
+	"github.com/aws/smithy-go"
 )
 
 const stackName = "swalker-test"
@@ -16,6 +18,8 @@ const stackName = "swalker-test"
 type client interface {
 	CreateStack(context.Context, *cloudformation.CreateStackInput, ...func(*cloudformation.Options)) (*cloudformation.CreateStackOutput, error)
 	UpdateStack(context.Context, *cloudformation.UpdateStackInput, ...func(*cloudformation.Options)) (*cloudformation.UpdateStackOutput, error)
+	DeleteStack(context.Context, *cloudformation.DeleteStackInput, ...func(*cloudformation.Options)) (*cloudformation.DeleteStackOutput, error)
+	DescribeStacks(context.Context, *cloudformation.DescribeStacksInput, ...func(*cloudformation.Options)) (*cloudformation.DescribeStacksOutput, error)
 }
 
 type Deployer struct {
@@ -32,7 +36,7 @@ func New(c client, tpl string) *Deployer {
 
 func (d *Deployer) Deploy(ctx context.Context) error {
 	// Try to create the stack
-	_, err := d.cf.CreateStack(ctx, &cloudformation.CreateStackInput{
+	res, err := d.cf.CreateStack(ctx, &cloudformation.CreateStackInput{
 		StackName:    aws.String(stackName),
 		TemplateBody: aws.String(d.tpl),
 	})
@@ -44,21 +48,82 @@ func (d *Deployer) Deploy(ctx context.Context) error {
 		}
 		return fmt.Errorf("error creating stack: %w", err)
 	}
+	d.waitForStack(ctx, *res.StackId)
 	return nil
 }
 
 func (d *Deployer) update(ctx context.Context) error {
 	log.Printf("updating stack")
-	_, err := d.cf.UpdateStack(ctx, &cloudformation.UpdateStackInput{
+	res, err := d.cf.UpdateStack(ctx, &cloudformation.UpdateStackInput{
 		StackName:    aws.String(stackName),
 		TemplateBody: aws.String(d.tpl),
 	})
 	if err != nil {
-		log.Fatalf("error updating stack: %v", err)
+		var ge *smithy.GenericAPIError
+		if errors.As(err, &ge) {
+			if ge.Message == "No updates are to be performed." {
+				return nil
+			}
+		}
+		return fmt.Errorf("error updating stack: %w", err)
 	}
+	d.waitForStack(ctx, *res.StackId)
 	return nil
 }
 
+func (d *Deployer) waitForStack(ctx context.Context, stackId string) error {
+	terminalStates := []types.StackStatus{
+		types.StackStatusCreateFailed,
+		types.StackStatusCreateComplete,
+		types.StackStatusRollbackFailed,
+		types.StackStatusRollbackComplete,
+		types.StackStatusDeleteFailed,
+		types.StackStatusDeleteComplete,
+		types.StackStatusUpdateComplete,
+		types.StackStatusUpdateRollbackFailed,
+		types.StackStatusUpdateRollbackComplete,
+		types.StackStatusImportComplete,
+		types.StackStatusImportRollbackFailed,
+		types.StackStatusImportRollbackComplete,
+	}
+	failureStates := []types.StackStatus{
+		types.StackStatusCreateFailed,
+		types.StackStatusRollbackFailed,
+		types.StackStatusDeleteFailed,
+		types.StackStatusUpdateRollbackFailed,
+		types.StackStatusImportRollbackFailed,
+	}
+
+	for {
+		res, err := d.cf.DescribeStacks(ctx, &cloudformation.DescribeStacksInput{
+			StackName: aws.String(stackName),
+		})
+		if err != nil {
+			return fmt.Errorf("error fetching stack status: %w", err)
+		}
+		if len(res.Stacks) != 1 {
+			return fmt.Errorf("unexpected number of stacks: %d", len(res.Stacks))
+		}
+		ss := res.Stacks[0].StackStatus
+		log.Printf("stack status: %s", ss)
+		for _, st := range terminalStates {
+			if st == ss {
+				for _, st2 := range failureStates {
+					if st2 == ss {
+						return fmt.Errorf("stack failure: %v", ss)
+					}
+				}
+				return nil
+			}
+		}
+		time.Sleep(5 * time.Second)
+	}
+}
+
 func (d *Deployer) Undeploy(ctx context.Context) error {
-	return nil
+	log.Printf("deleting stack")
+	_, err := d.cf.DeleteStack(ctx, &cloudformation.DeleteStackInput{
+		StackName: aws.String(stackName),
+	})
+	return err
 }
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/aws/aws-sdk-go-v2 v1.3.2
 	github.com/aws/aws-sdk-go-v2/config v1.1.5
 	github.com/aws/aws-sdk-go-v2/service/cloudformation v1.3.1
+	github.com/aws/smithy-go v1.3.1 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/stretchr/testify v1.7.0
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
